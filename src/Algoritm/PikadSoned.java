264701d8cebc4ea37088415c123ec11d7ed43fe6
Rudolf112 <rudolfpurge112@gmail.com>
2017-01-28 11:11:56 +0200

    PikadSoned lahendatud

--- src/Algoritm/PikadSoned.java
+++ src/Algoritm/PikadSoned.java
@@ -1,12 +1,26 @@
 package Algoritm;
 
-import java.util.Arrays;
-
 /**
  * Antud on massiiv. Mitu sõne on massiivis keskmisest pikemad?
  */
 public class PikadSoned {
+    public static void main(String[] args) {
+        double summa = 0;
+        int counter = 0;
+        String[] naide = {"kaalikas", "joonas", "maakera", "homeros", "mandel"}; // vastus on 3
+        for (int i = 0; i < naide.length ; i++) {
+            summa += naide[i].length();
 
-    String[] naide = {"kaalikas", "joonas", "maakera", "homeros", "mandel"}; // vastus on 3
+        }
+        double keskmine = summa/naide.length;
+
+        for (int i = 0; i < naide.length; i++) {
+            if (naide[i].length()>keskmine){
+                counter += 1;
+            }
+        }
+        System.out.println(counter);
+    }
+
 
 }
