15eaecf180b05451e52fca2f03f78dbafdb8e617
Rudolf112 <rudolfpurge112@gmail.com>
2017-01-28 11:44:28 +0200

    Tagaajamine jms

--- src/JavaFX/Tagaajamine.java
+++ src/JavaFX/Tagaajamine.java
@@ -1,5 +1,10 @@
 package JavaFX;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
 /**
  * Joonista 500x500 ekraanile ring. Kui hiir ringile vastu läheb, leiab ring omale
  * uue koha. Ei pea animeerima ega midagi fäänsit. Lihtsalt ilmub uues kohas ja kõik.
@@ -7,4 +12,21 @@ package JavaFX;
  * mitte klikist. Aga alustada võid muidugi klikist.
  */
 public class Tagaajamine {
+    Circle ring;
+    public Tagaajamine() {meetod();}
+
+    private void meetod(){
+        Stage stage = new Stage();
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 500, 500);
+
+        ring = new Circle(Math.random()*500, Math.random()*500, 30);
+        ring.setOnMouseMoved( event -> {
+            ring.setCenterX(Math.random()*500);
+            ring.setCenterY(Math.random()*500);
+        });
+        pane.getChildren().add(ring);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
